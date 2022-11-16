56eb6450646a45d29e71629d8bc5e72785e4f1b9
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2022-11-16 13:44:06 +0000

    Configure vite dev server port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  server: {
+    port: 4000,
+    strictPort: true
+  },
   plugins: [
     wasm(),
     topLevelAwait()
